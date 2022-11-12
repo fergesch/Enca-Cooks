d03f33325defdae467519c452808b208b006d3e8
Michael Ferguson <mikeferg2011@gmail.com>
2022-11-11 21:27:15 -0600

    basic vue js start

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup>
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
@@ -8,7 +8,7 @@ import HelloWorld from "./components/HelloWorld.vue";
     <img
       alt="Vue logo"
       class="logo"
-      src="@/assets/logo.svg"
+      src="@/assets/fergesch_logo.svg"
       width="125"
       height="125"
     />
